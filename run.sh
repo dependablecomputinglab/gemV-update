03831124cd40ee403166670e1434dca87c4b946a
Yohan Ko <yohan.ko@yonsei.ac.kr>
2017-01-10 18:39:00 +0900

    add run script file

---
+++ run.sh
@@ -0,0 +1,73 @@
+bench_home="/home/yohan/miBench"
+
+case "$2" in
+hello )
+  bench="helloWorld/hello_$1"
+  options=""
+  ;;
+matmul )
+  bench="matrixmul/matmul_$1"
+  options=""
+  ;;
+qsort )
+  bench="automotive/qsort/qsort_small_$1"
+  options="$bench_home/automotive/qsort/input_small.dat"
+  ;;
+stringsearch )
+  bench="office/stringsearch/search_small_$1"
+  options=""
+  ;;
+gsm )
+  bench="telecomm/gsm/bin/toast_$1"
+  options="-fps -c $bench_home/telecomm/gsm/data/small.au"
+  ;;
+bitcount )
+  bench="automotive/bitcount/bitcnts_$1"
+  options=75000
+  ;;
+jpeg )
+  bench="consumer/jpeg/jpeg-6a/cjpeg_$1"
+  options="-dct int -progressive -opt -outfile output_small_encode.jpeg $bench_home/consumer/jpeg/input_small.ppm"
+  ;;
+fft )
+  bench="telecomm/FFT/fft_$1"
+  options="4 4096"
+  ;;
+dijkstra )
+  bench="network/dijkstra/dijkstra_small_$1"
+  options="$bench_home/network/dijkstra/input.dat"
+  ;;
+basicmath ) 
+  bench="automotive/basicmath/basicmath_small_$1"
+  options=""
+  ;;
+typese )
+  bench="/consumer/typeset/lout-3.24/lout_$1"
+  options=" -I $bench_home/consumer/typeset/lout-3.24/include -D $bench_home/consumer/typeset/lout-3.24/data -F $bench_home/consumer/typeset/lout-3.24/font -C $bench_home/consumer/typeset/lout-3.24/maps -H $bench_home/consumer/typeset/lout-3.24/hyph $bench_home/consumer/typeset/small.lout"
+  ;;
+crc )
+  bench="telecomm/CRC32/crc_$1"
+  options="$bench_home/telecomm/adpcm/data/large.pcm"
+  ;;
+patricia )
+  bench="network/patricia/patricia_$1"
+  options="$bench_home/network/patricia/small.udp"
+  ;;
+sha )
+  bench="security/sha/sha_$1"
+  options="$bench_home/security/sha/input_small.asc"
+  ;;
+ispell )
+  bench="office/ispell/ispell_$1"
+  options="$bench_home/office/ispell/tests/americanmed+ < $bench_home/office/ispell/tests/small.txt"
+  ;;
+esac
+
+protection=no_protection								# Protection scheme to be used
+vul_analysis=no								# Enable/Disable vulnerability analysis
+cpu_type=arm_detailed								# CPU Type
+num_procs=1									# Number of processors
+gemv_exec_path=./build/$1/gem5.opt		# Path to gemv executable
+config_path=./configs/example/se.py		# Path to config file
+
+$gemv_exec_path -d $2 -re --stdout-file=simout --stderr-file=simerr $config_path --cpu-type=$cpu_type --caches -n $num_procs -c "$bench_home/$bench" -o "$options" --cpu-type=$cpu_type --caches --vul_analysis=yes --vul_params=params.in --output=$2/result
